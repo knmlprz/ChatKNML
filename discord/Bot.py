4cb0854fbe8e7f11c474e1e1205c8aa87c01fd28
Mikołaj Data <miki3867@gmail.com>
2023-10-30 21:09:46 +0100

    Create !ask command and create buttons

--- discord/Bot.py
+++ discord/Bot.py
@@ -1,10 +1,30 @@
 import discord
+import os
+import config
+
 from discord.ext import commands
 
 
 intents = discord.Intents.all()
 
 
-client = commands.Bot(command_prefix="!", help_command=None, intents=intents)
+bot = commands.Bot(command_prefix=config.PREFIX,
+                      help_command=None, intents=intents)
 
-client.run()
+
+class Buttons(discord.ui.View):
+    def __init__(self, *, timeout=180):
+        super().__init__(timeout=timeout)
+
+    @discord.ui.button(label="Good bot",style=discord.ButtonStyle.green)
+    async def good_button(self, interaction:discord.Interaction, button:discord.ui.Button):
+        await interaction.response.send_message(content=f"This is an edited button response!")
+
+    @discord.ui.button(label="Bad bot",style=discord.ButtonStyle.red)
+    async def bad_button(self, interaction:discord.Interaction, button:discord.ui.Button):
+        await interaction.response.send_message(content=f"This is an edited button response!")
+@bot.command()
+async def ask(ctx, *args):
+    await ctx.send(args, view=Buttons())
+
+bot.run(os.environ.get('TOKEN', ''))

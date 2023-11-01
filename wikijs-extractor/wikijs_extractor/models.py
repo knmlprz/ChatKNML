d904b7f6888e18baa0b8a3a0f41111c8aae416d8
Piotr Krawiec <piotr.krawiec23@gmail.com>
2023-11-01 18:25:52 +0100

    feat: Page to langchain Document conv

--- wikijs-extractor/wikijs_extractor/models.py
+++ wikijs-extractor/wikijs_extractor/models.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel, ConfigDict, HttpUrl, computed_field
 from urllib.parse import urljoin
 
+from langchain.docstore.document import Document
+
 class PageListItem(BaseModel):
     # Allow extra fields
     model_config = ConfigDict(extra="allow")
@@ -24,6 +26,20 @@ class Page(BaseModel):
     
     @computed_field
     @property
-    def url(self) -> HttpUrl:
+    def source(self) -> HttpUrl:
         # Url pf page is instance url / locale / path
         return HttpUrl(urljoin(self.instance_url, f"{self.locale}/{self.path}"))
+    
+    def to_document(self) -> Document:
+        return Document(
+            page_content=self.content,
+            metadata={
+                "id": self.id,
+                "path": self.path,
+                "title": self.title,
+                "description": self.description,
+                "locale": self.locale,
+                "instance_url": self.instance_url,
+                "source": self.source,
+            }
+        )

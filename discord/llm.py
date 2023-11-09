2de7c108d2eefde38a4d8c19e867079a1835c4b1
Piotr Krawiec <164165@stud.prz.edu.pl>
2023-11-09 16:38:48 +0100

    fix: API endpoints

--- discord/llm.py
+++ discord/llm.py
@@ -3,23 +3,25 @@ from openai import AsyncOpenAI
 
 smol_model_client = AsyncOpenAI(
     api_key="XD",
-    base_url="http://host.docker.internal:8001",
+    base_url="http://host.docker.internal:8001/v1",
 )
 
 big_model_client = AsyncOpenAI(
     api_key="XD",
-    base_url="http://host.docker.internal:8000",
+    base_url="http://host.docker.internal:8000/v1",
 )
 
 PROMPT = """
 Jesteś AI działającym na discordzie Koła Naukowego Machine Learning Politechniki Rzeszowskiej.
-W skrócie (KNML PRz). Jesteś tutaj, aby pomóc ludziom w ich problemach związanych z ML. 
+W skrócie (KNML PRz). Jesteś tutaj, aby im pomagać. 
 
 1. Bądź miły i uprzejmy.
 2. Każdą odpowiedź zakończ emotką.
+3. Odpowiadaj w języku polskim.
+4. Przerwij pisanie po udzieleniu odpowiedzi. 
 
-Pytanie: {}
-Odpowiedź: 
+Człowiek: {}
+AI:  
 """
 
 
@@ -31,12 +33,13 @@ async def ask_model(question: str, model: Literal["smol", "big"]):
         case "big":
             client = big_model_client
 
+    prompt = PROMPT.format(question)
+    print(prompt)
+
     response = await client.completions.create(
         model="DOESNT MATTER",  # Literally doesn't matter
-        prompt=PROMPT.format(question),
-        max_tokens=200,
-        temperature=0.7,
-        n=1,  # Number of completions to generate
+        prompt=prompt,
+        max_tokens=500,
     )
 
     return response.choices[0].text

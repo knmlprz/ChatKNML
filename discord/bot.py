7964db7f33c57dde9886a3bd224fd83c628abaf1
SzymonBal <szymekuj@interia.pl>
2023-11-29 20:05:24 +0100

    Saving conversations in txt files

--- discord/bot.py
+++ discord/bot.py
@@ -23,6 +23,12 @@ class Buttons(discord.ui.View):
         await interaction.response.send_message(
             content="This is an edited button response!"
         )
+        filename = f"discord/private_chats/{interaction.user.id}.txt"
+        with open(filename, "a") as file:
+            file.write(f"1\n")
+        self.stop()
+
+
 
     @discord.ui.button(label="Bad bot", style=discord.ButtonStyle.red)
     async def bad_button(
@@ -33,6 +39,10 @@ class Buttons(discord.ui.View):
         await interaction.response.send_message(
             content="This is an edited button response!"
         )
+        filename = f"discord/private_chats/{interaction.user.id}.txt"
+        with open(filename, "a") as file:
+            file.write(f"0\n")
+        self.stop()
 
 
 @bot.command()
@@ -43,4 +53,31 @@ async def ask(
     await ctx.send(args, view=Buttons())
 
 
-bot.run(os.environ["TOKEN"])
+# wywołanie bota i przejście do rozmowy prywatnej
+@bot.command()
+async def DM(ctx, *, message=None):
+    if not isinstance(ctx.channel, discord.DMChannel):
+        message = "Witam, w czym mogę pomóc"
+        author = ctx.author
+        await author.send(message)
+    else:
+        message = "Jesteś już w rozmowie prywatnej, w czym mogę pomóc"
+        author = ctx.author
+        await author.send(message) 
+
+# odpowiedź na każde z pytań lorem ipsum
+@bot.event
+async def on_message(message):
+    if (message.author.bot == False) and isinstance(message.channel, discord.DMChannel) and not message.content.startswith('!DM'): 
+                response = "Lorem ipsum \n"
+                await message.channel.send(response, view = Buttons())
+                filename = f"discord/private_chats/{message.author.id}.txt"
+                with open(filename, "a") as file:
+                        file.write(f"{message.author.display_name} - {message.clean_content} \n")
+                        file.write(f"odpowiedź bota {response} - ")
+    
+    await bot.process_commands(message)
+
+
+
+bot.run("MTE3NTUwNDAwMTc4NjE4NzgxNg.GeJlgw.at5Pn28P4hiYlKssHgzNyz2l80oQIwOjIeMG_k")

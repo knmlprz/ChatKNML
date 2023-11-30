082c8efc68737a01fac9fe38f77152a945cc3a01
Piotr Krawiec <164165@stud.prz.edu.pl>
2023-11-30 09:17:29 +0100

    fix(discord): Remove admin intents

--- discord/chatbot/bot.py
+++ discord/chatbot/bot.py
@@ -8,7 +8,8 @@ from discord.ext import commands
 
 from chatbot import config, llm
 
-intents = discord.Intents.all()
+intents = discord.Intents.default()
+intents.message_content = True
 
 bot = commands.Bot(command_prefix=config.PREFIX, help_command=None, intents=intents)
 

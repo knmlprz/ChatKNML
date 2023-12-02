ed199e1f23f24805940601f23399351fda740fcc
Mikołaj Data <miki3867@gmail.com>
2023-12-02 10:59:03 +0100

    Clone boilerplate

---
+++ api/drfboilerplate/settings/dev.py
@@ -0,0 +1,4 @@
+from drfboilerplate.settings.prod import *  # noqa: F403, F401
+
+DEBUG = True
+CORS_ALLOWED_ORIGINS = []
---
+++ api/drfboilerplate/settings/local.py
@@ -0,0 +1,4 @@
+from drfboilerplate.settings.prod import *  # noqa: F403, F401
+
+DEBUG = True
+CORS_ALLOW_ALL_ORIGINS = True

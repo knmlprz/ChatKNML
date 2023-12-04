ff037f150614a91f9faa7b34f2c4d3a94450d682
annbbbb <annbebenn@gmail.com>
2023-12-04 20:14:01 +0100

    Fix api/api/wsgi.py and api/api/urls.py

--- api/api/wsgi.py
+++ api/api/wsgi.py
@@ -11,10 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-<<<<<<< HEAD
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
-=======
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
->>>>>>> main
+
 
 application = get_wsgi_application()

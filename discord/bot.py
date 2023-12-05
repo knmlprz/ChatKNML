7c979d8a586b62f996f87d24381c1abbff7e8fe2
SzymonBal <szymekuj@interia.pl>
2023-12-05 22:39:15 +0100

    Delete show function, change name of get_chats for get_chats_history

--- discord/bot.py
+++ discord/bot.py
@@ -38,25 +38,15 @@ class Buttons(discord.ui.View):
 
 import asyncio
 from collections import defaultdict
-async def chats_history(bot, last_messages_per_channel):
+async def get_chats_history(bot):
+    chats_history = defaultdict(list)
     for guild in bot.guilds:
         for channel in guild.text_channels:
             if channel.permissions_for(guild.me).read_messages:
                 async for message in channel.history(limit=100):
-                    last_messages_per_channel[channel.id].append(f"{message.author.name}: {message.content}")
+                    chats_history[channel.id].append(f"{message.author.name}: {message.content}")
+    return chats_history
                     
-                                        
-
-@bot.command()
-async def show(ctx: commands.Context, limit: int = 10):
-    last_messages = await start_bot(ctx.bot)
-    channel_id = ctx.channel.id
-    if last_messages[channel_id]:
-        for msg in last_messages[channel_id][:limit]:
-            await ctx.send(msg)
-    else:
-        await ctx.send("Brak ostatnich wiadomości.")
-
-
+                                    
 bot.run("MTE3NTUwNDAwMTc4NjE4NzgxNg.GPMwqY.Sm3nWVukPsQhF3eeAdKQWVaLSPgrJZj2qSHCW8")
 

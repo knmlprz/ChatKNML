bede6e057c02d2ece3dce087eb456fb55938aab8
Mikołaj Data <miki3867@gmail.com>
2024-01-08 23:17:10 +0100

    Implement model

--- api/documents/models.py
+++ api/documents/models.py
@@ -1,30 +1,16 @@
 from django.db import models
-from ninja import ModelSchema
-from typing import List
-from pydantic import BaseModel
-
-class ChunkBase(BaseModel):
-    text: str
-    embedding: List[float]
-    chunk_idx: int
-    start_char_idx: int
-    end_char_idx: int
-
-class Chunk(ChunkBase):
-    id: int
-
-    class Config:
-        orm_mode = True
-
-class DocumentBase(BaseModel):
-    text: str
-    embedding: List[float]
-    chunks: List[ChunkCreate]
 
 
-class Document(DocumentBase):
-    id: int
-    chunks: List[Chunk]
+class Chunk(models.Model):
+    text = models.CharField(max_length=100)
+    # TODO: Change this
+    embedding = models.CharField(max_length=100)
+    chunk_idx = models.IntegerField()
+    start_char = models.IntegerField()
+    end_char = models.IntegerField()
 
-    class Config:
-        orm_mode = True
+
+class Document(models.Model):
+    text = models.TextField()
+    embedding = models.CharField(max_length=100)
+    chunks = models.ForeignKey(Chunk, on_delete=models.CASCADE)

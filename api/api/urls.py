85d2f75dbe9748cf83c360c402dae2aba2351e0a
Mikołaj Data <miki3867@gmail.com>
2024-01-12 21:14:57 +0100

    Delete .gitignore, add pgvector update model

--- api/api/urls.py
+++ api/api/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("documents/", include("documents.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- api/documents/models.py
+++ api/documents/models.py
@@ -1,10 +1,10 @@
 from django.db import models
+from pgvector.django import VectorField
 
 
 class Chunk(models.Model):
     text = models.CharField(max_length=100)
-    # TODO: Change this
-    embedding = models.CharField(max_length=100)
+    embedding = VectorField(dimensions=10)
     chunk_idx = models.IntegerField()
     start_char = models.IntegerField()
     end_char = models.IntegerField()
@@ -12,5 +12,5 @@ class Chunk(models.Model):
 
 class Document(models.Model):
     text = models.TextField()
-    embedding = models.CharField(max_length=100)
-    chunks = models.ForeignKey(Chunk, on_delete=models.CASCADE)
+    embedding = VectorField(dimensions=10)
+    chunks = models.ForeignKey(Chunk, on_delete=models.CASCADE, null=True, blank=True)

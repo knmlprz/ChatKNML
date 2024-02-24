425c5bf821874d443e921a5211da05f36ec3002f
Kleczyk <mailnasyff@gmail.com>
2024-02-24 13:06:54 +0100

    update test_llm.py and .gitignore

--- llm/test/test_llm.py
+++ llm/test/test_llm.py
@@ -4,19 +4,17 @@ import pytest
 import requests
 
 
+@pytest.mark.llm
 def test_llm_response():
-    url = 'http://0.0.0.0:9000/v1/completions'
-  
+    url = "http://0.0.0.0:9000/v1/completions"
+
     prompt = "co jest stolicą polski?"
-    stop_signs = ["\n", "###"]
-    data = {
-        'prompt': prompt,
-        'stop': stop_signs
-    }
+    stop_signs = ("\n", "###")
+    data = {"prompt": prompt, "stop": stop_signs}
 
     response = requests.post(url, json=data)
-    
+
+    resp_body = response.json()
     assert response.status_code == 200
-	resp_body = response.json()
-    assert 'choices' in resp_body
-    assert 'text' in resp_body['choices'][0]
+    assert "choices" in resp_body
+    assert "text" in resp_body["choices"][0]

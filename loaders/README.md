0c1cd95800e2b867cb02df12ffa2d9ae1f8e9895
Bisewicz <107929119+Bisewicz@users.noreply.github.com>
2024-02-26 19:05:01 +0100

    Add local data loader and tests for both loaders

---
+++ loaders/README.md
@@ -0,0 +1,33 @@
+# Loaders Module
+
+This module contains data loaders for the ChatKNML chatbot.
+
+## Installation
+
+To install the loaders module, run the following command:
+
+```bash
+poetry install
+```
+
+## Testing
+
+To run tests, run the following command:
+
+```bash
+poetry run pytest
+```
+
+## Formatting and linting
+
+Formatting:
+
+```bash
+poetry run black .
+```
+
+Linting
+
+```bash
+poetry run flake8
+```
--- loaders/clean_web_loader.py
+++ loaders/clean_web_loader.py
@@ -5,9 +5,27 @@ from newspaper import Article
 from typing import Union, List, Optional
 from functools import reduce
 
+
 class CleanWebLoader(BaseLoader):
+    """
+    A class for loading web content, extracting and cleaning text using the 'newspaper' library,
+    and converting it into a specific data structure.
+
+    Attributes:
+        url_list (Union[List[str], str]): Either a string or a list of strings representing URLs.
+        depth (int): Maximum depth for recursive extraction (default is 1).
+
+    Methods:
+        newspaper_extractor(html): Extracts and cleans text content from HTML using the 'newspaper' library.
+        ds_converter(docs): Converts a list of documents into a specific data structure.
+        junk_remover(docs): Identifies and returns a list of suspected junk documents based on specific criteria.
+        load(): Loads web content from specified URLs, extracts text, and converts it into a specific data structure.
+        load_and_split(text_splitter, chunk, chunk_overlap): Loads web content, splits it into chunks, and converts it into a specific data structure.
+
+    """
+
     article = Article("")
-    
+
     def __init__(self, url_list: Union[List[str], str], depth: int = 1):
         """
         Initializes the CleanWebLoader instance.
@@ -29,18 +47,18 @@ class CleanWebLoader(BaseLoader):
         """
         CleanWebLoader.article.set_html(html)
         CleanWebLoader.article.parse()
-        return ' '.join(CleanWebLoader.article.text.split())
-        
+        return " ".join(CleanWebLoader.article.text.split())
+
     @staticmethod
     def ds_converter(docs):
         """
         Converts a list of documents into a specific data structure.
 
         :param docs: List of documents to be converted.
-        :return: List of dictionaries, each representing a document with 'text' and 'url' keys.
+        :return: List of dictionaries, each representing a document with 'text' key.
         """
-        return [{'text': doc.page_content} for doc in docs]
-    
+        return [{"text": doc.page_content} for doc in docs]
+
     @staticmethod
     def junk_remover(docs):
         """
@@ -51,40 +69,53 @@ class CleanWebLoader(BaseLoader):
         :return: A list of suspected junk documents based on the criteria of having less than 300 characters
                 or having the same text as another document in the input list.
         """
-        short_docs = [doc for doc in docs if len(doc.page_content) < 300]
-        junk_docs = list(short_docs)
+        junk_docs = [doc for doc in docs if len(doc.page_content) < 300]
         seen_texts = set()
+        clear_docs = []
         for doc in docs:
-            if doc.page_content in seen_texts and doc not in junk_docs:
+            if "title" not in doc.metadata.keys():
                 junk_docs.append(doc)
-            elif 'title' not in doc.metadata.keys():
-                junk_docs.append(doc)
-            else:
+            elif doc.page_content not in seen_texts and doc not in junk_docs:
+                clear_docs.append(doc)
                 seen_texts.add(doc.page_content)
-        return [doc for doc in docs if doc not in junk_docs]
-        
+            else:
+                pass
+        return clear_docs
+
     def load(self) -> List[dict]:
         """
         Loads web content from specified URLs, extracts text using the 'newspaper' library,
         and converts it into a specific data structure using the ds_converter and junk_remover methods.
 
-        :return: List of dictionaries, each representing a document with 'text' and 'url' keys.
+        :return: List of dictionaries, each representing a document with 'text' key.
         """
         docs = []
         if isinstance(self.url_list, str):
             self.url_list = [self.url_list]
         for address in self.url_list:
             try:
-                loader = RecursiveUrlLoader(url=address, max_depth=self.depth, 
-                                            extractor=CleanWebLoader.newspaper_extractor)
+                loader = RecursiveUrlLoader(
+                    url=address,
+                    max_depth=self.depth,
+                    extractor=CleanWebLoader.newspaper_extractor,
+                )
                 docs.extend(loader.load())
             except Exception as e:
                 print(f"Exception: {e}")
                 break
-        docs = reduce(lambda data, method: method(data), [CleanWebLoader.junk_remover, CleanWebLoader.ds_converter], docs)
+        docs = reduce(
+            lambda data, method: method(data),
+            [CleanWebLoader.junk_remover, CleanWebLoader.ds_converter],
+            docs,
+        )
         return docs
-    
-    def load_and_split(self, text_splitter: Optional[TextSplitter] = None, chunk: int = 400, chunk_overlap: int = 80) -> List[dict]:
+
+    def load_and_split(
+        self,
+        text_splitter: Optional[TextSplitter] = None,
+        chunk: int = 400,
+        chunk_overlap: int = 80,
+    ) -> List[dict]:
         """
         Loads web content from specified URLs, extracts text using the 'newspaper' library,
         splits it into chunks using the provided or default TextSplitter,
@@ -93,12 +124,20 @@ class CleanWebLoader(BaseLoader):
         :param text_splitter: Optional TextSplitter instance to use for splitting documents.
         :param chunk: Chunk size for text splitting (default is 400).
         :param chunk_overlap: Overlap size between chunks (default is 80).
-        :return: List of dictionaries, each representing a document with 'text' and 'url' keys.
+        :return: List of dictionaries, each representing a document with 'text' key.
         """
         if text_splitter is None:
-            _text_splitter: TextSplitter = SpacyTextSplitter(pipeline='pl_core_news_sm', chunk_size=chunk, chunk_overlap=chunk_overlap)
+            _text_splitter: TextSplitter = SpacyTextSplitter(
+                pipeline="pl_core_news_sm",
+                chunk_size=chunk,
+                chunk_overlap=chunk_overlap,
+            )
         else:
             _text_splitter = text_splitter
         docs = self.load()
-        docs = reduce(lambda data, method: method(data), [CleanWebLoader.junk_remover, CleanWebLoader.ds_converter], docs)
+        docs = reduce(
+            lambda data, method: method(data),
+            [CleanWebLoader.junk_remover, CleanWebLoader.ds_converter],
+            docs,
+        )
         return _text_splitter.split_documents(docs)

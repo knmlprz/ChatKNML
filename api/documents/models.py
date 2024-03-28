5b66dd0cfd270122e842b145ed1981e4332462c9
bafaurazan <rafalbe777@gmail.com>
2024-03-28 14:40:29 +0100

    deleting embedding from document

--- api/documents/models.py
+++ api/documents/models.py
@@ -4,4 +4,3 @@ from pgvector.django import VectorField
 
 class Document(models.Model):
     text = models.TextField[str, str]()
-    embedding = VectorField[list[float], VectorField](dimensions=10)
--- api/documents/schemas.py
+++ api/documents/schemas.py
@@ -3,10 +3,8 @@ from ninja import Schema
 
 class DocumentIn(Schema):
     text: str
-    embedding: list[float]
 
 
 class DocumentOut(Schema):
     id: int
     text: str
-    embedding: list[float]

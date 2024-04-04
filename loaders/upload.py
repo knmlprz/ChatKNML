1ab17eb746b2b487eab60012c4043c4ba08da56e
bisd98 <107929119+bisd98@users.noreply.github.com>
2024-04-04 17:47:15 +0200

    feat: Implement upload_data function using REST API endpoint

---
+++ loaders/upload.py
@@ -0,0 +1,18 @@
+import clean_web_loader, local_data_loader
+import requests
+
+
+def upload_data(web_url=None, web_depth=None, local_path=None):
+    url = 'http://localhost:8000/api/document/'
+    docs = []
+    
+    if web_url:
+        web_loader = clean_web_loader.CleanWebLoader(web_url, depth=web_depth)
+        docs.extend(web_loader.load())
+        
+    if local_path:
+        local_loader = local_data_loader.LocalDataLoader(local_path)
+        docs.extend(local_loader.load())
+
+    for doc in docs:
+        requests.post(url, json=doc)

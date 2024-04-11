8b86878811b878fd5de5e75e7c75f93da453fd67
Anna Bęben <annbebenn@gmail.com>
2024-04-12 00:52:09 +0200

    Generate chunks together with document creation (#123)

--- api/chunks/utils.py
+++ api/chunks/utils.py
@@ -1,19 +1,26 @@
-def split_document_into_chunks(document, chunk_size):
+from documents.models import Document
+
+
+def split_document_into_chunks(document: Document, chunk_size: int = 100) -> list[dict]:
+    """Splits document into chunks of size chunk_size and returns them as dictionaries.
+    End_char is assumed to be the last included character of the document."""
     chunks = []
     start_char = 0
     end_char = chunk_size
     chunk_idx = 1
 
-    while start_char < len(document):
-        if end_char >= len(document):
-            end_char = len(document)
-        chunk_text = document[start_char:end_char]
-        chunk = {
-            'text': chunk_text,
-            'chunk_idx': chunk_idx,
-            'start_char': start_char,
-            'end_char': end_char
-        }
+    while start_char < len(document.text):
+        if end_char >= len(document.text):
+            end_char = len(document.text)
+        chunk_text = document.text[start_char:end_char]
+        chunk = dict(
+            text=chunk_text,
+            chunk_idx=chunk_idx,
+            start_char=start_char,
+            end_char=(end_char - 1),
+            document_idx=document.id,
+        )
+
         chunks.append(chunk)
 
         start_char += chunk_size
--- api/documents/controllers.py
+++ api/documents/controllers.py
@@ -1,13 +1,27 @@
 from http import HTTPStatus
 
+from django.db import transaction
+from chunks.utils import split_document_into_chunks
+from chunks.models import Chunk
 from documents.models import Document
 from documents.schemas import DocumentIn, DocumentOut
 
 
 def create_document_controller(payload: DocumentIn) -> tuple[HTTPStatus, DocumentOut]:
-    document = Document(**payload.dict())
-    document.full_clean()
-    document.save()
+    """Creating the document and chunks of that document.
+    Making sure that both chunks and document are created by using atomic transaction.
+    """
+    with transaction.atomic():
+        document = Document(**payload.dict())
+        document.full_clean()
+        document.save()
+
+        chunks = split_document_into_chunks(document, 100)
+        for chunk_data in chunks:
+            chunk = Chunk(**chunk_data)
+            chunk.full_clean()
+            chunk.save()
+
     return HTTPStatus.CREATED, document
 
 
@@ -21,11 +35,23 @@ def retrieve_document_controller(id: int) -> DocumentOut:
 
 
 def update_document_controller(payload: DocumentIn, id: int) -> DocumentOut:
-    document = Document.objects.get(id=id)
-    for attr, value in payload.dict().items():
-        setattr(document, attr, value)
-    document.full_clean()
-    document.save()
+    with transaction.atomic():
+        document = Document.objects.get(id=id)
+        for attr, value in payload.dict().items():
+            setattr(document, attr, value)
+        document.full_clean()
+        document.save()
+
+        # Removing old chunks related to this document first and then
+        # generating new ones. Because new
+        # embeddings will have to be generated there
+        Chunk.objects.filter(chunks__document_idx=document).delete()
+        # Create new chunks then
+        chunks = split_document_into_chunks(document, 100)
+        for chunk_data in chunks:
+            chunk = Chunk(**chunk_data)
+            chunk.full_clean()
+            chunk.save()
     return document
 
 

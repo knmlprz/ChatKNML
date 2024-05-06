35bc9952b6016a85e41e137ae831d4002bd76ba5
annbbbb <annbebenn@gmail.com>
2024-05-06 16:41:35 +0200

    Added efficient saving of document chunkss

--- api/documents/controllers.py
+++ api/documents/controllers.py
@@ -16,7 +16,7 @@ def create_document_controller(payload: DocumentIn) -> tuple[HTTPStatus, Documen
         document.full_clean()
         document.save()
 
-        chunks = split_document_into_chunks(document, 100) 
+        chunks = split_document_into_chunks(document, 100)
         chunk_instances = [Chunk(**chunk_data) for chunk_data in chunks]
         Chunk.objects.bulk_create(chunk_instances)
 
